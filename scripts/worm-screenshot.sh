d186e46ecafcc8e582a02ace7f30817b0dc994d6
cryptiddddd <khavari.bird@gmail.com>
2026-06-03 13:10:15 -0700

    improved customizability

--- scripts/worm-screenshot.sh
+++ scripts/worm-screenshot.sh
@@ -7,16 +7,20 @@ screenshot utility. recommended to make a convenient keybind for yourself.
 # shellcheck disable=SC1091
 source "/usr/local/lib/wormboy-utils"
 
+# configs.. future args
+IMG_FMT="%w × %h"
+DISPLAY_BIN="display"
+
 
 function displayclipboard() {
     log "displaying image"
     # xclip -o -selection -clipboard -t image/png | xviewer
-    display <(xclip -o -t image/png -selection clipboard)
+    "$DISPLAY_BIN" <(xclip -o -t image/png -selection clipboard)
     # xclip -o -selection clipboard | feh
 }
 
 function showsuccess() {
-    dimensions=$(identify -format "%w × %h" <(xclip -o -selection clipboard))
+    dimensions=$(identify -format "$IMG_FMT" <(xclip -o -selection clipboard))
 
     action=$(dunstify -u "LOW" -i edit-paste -a "screenshot" "copied to clipboard" "png, ($dimensions)" --action "view,view")
     if [[ "$action" == "view" ]]; then
@@ -31,7 +35,7 @@ function screenshot() {
 
  
 if ! screenshot ; then
-    log "screenshot cancelled by user."
+    warn "screenshot cancelled by user."
     exit 1
 else
     showsuccess
